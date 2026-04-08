e65b39599e6cc3d28384ec427e798f70cb9e97d8
Sanjeev Midda <s.midda1@gmail.com>
2026-04-08 21:52:54 +0100

    created memory to hold counter value, and functions for increaseing, decreasing and resetting the counter value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
+// create a counter app
+// should include functionalities to increase, decrease and reset
+// deal with edge cases - what if it goes below 0?
+
+// somewhere to store our counter value - useState
+// functions to increase, decrease and reset
+// buttons to increase, decrease and reset
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => setCounter(counter + 1);
+  const decrease = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
+
   return <div className="App"></div>;
 }
 
